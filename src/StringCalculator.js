6c6c5f02f1fa01c137e0558676ce994650e29ada
pshin <iuliiap@ya.ru>
2015-11-28 21:39:48 +0200

    Refactoring. Getting only numbers

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -4,30 +4,12 @@
 function StringCalculator () {}
 
 StringCalculator.prototype.add = function (stringNumbers) {
-    stringNumbers = stringNumbers.replace(/(^\/\/\[?(.*)]?\n)/, '');
-
-    var delimitter = RegExp.$2;
-
-    if (delimitter.match(/\[|]/g)) {
-        delimitter
-            .split(/\[|]/)
-            .forEach(function (element) {
-                while (element && stringNumbers.indexOf(element) !== -1) {
-                    stringNumbers = stringNumbers.replace(element, ',');
-                }
-            });
-
-        delimitter = false;
-    }
-
-    return stringNumbers
-        .split(delimitter || /,|\n/g)
+    return (stringNumbers.match(/[-\d]+/g) || [])
         .reduce(function (previousValue, currentValue) {
             currentValue = Number(currentValue);
             if (!Number.isInteger(currentValue) || currentValue > 1000) {
                 currentValue = 0;
-            }
-            if (currentValue < 0) {
+            } else if (currentValue < 0) {
                 throw new Error('negatives not allowed');
             }
             return previousValue + currentValue;
